288dd78705459e9f0014ad2561b81f32ab375a3e
frank soboczenski <frank.soboczenski@gmail.com>
2018-12-15 00:06:57 +0000

    added first python UI lecture and code

---
+++ 6_Development/Python/Python_UI/Part1/1FirstGUI.py
@@ -0,0 +1,16 @@
+
+#======================
+# imports
+#======================
+import tkinter as tk
+
+# Create instance
+win = tk.Tk()
+
+# Add a title
+win.title("Python GUI")
+
+#======================
+# Start GUI
+#======================
+win.mainloop()
